90ad28fd726be270211ffe4210392dee4bcbf9a3
Mohandas Albuquerque  |  Sell Administradora <mohandas.albuquerque@selladm.com.br>
2026-07-24 10:33:12 -0300

    Apenas descomentei o codigo que subi comentado

--- def.py
+++ def.py
@@ -1,31 +1,31 @@
-# from tkinter import messagebox, simpledialog, Tk
+from tkinter import messagebox, simpledialog, Tk
 
-# def cafeteria():
-#     print("--- MENU DA CAFETERIA ---")
-#     print("1. cafe puro")
-#     print("2. cafe com leite")
-#     print("3. chocolate")
-#     print("4. cha")
-#     print("5. suco")
-#     print("-------------------------")
+def cafeteria():
+    print("--- MENU DA CAFETERIA ---")
+    print("1. cafe puro")
+    print("2. cafe com leite")
+    print("3. chocolate")
+    print("4. cha")
+    print("5. suco")
+    print("-------------------------")
     
-#     try:
-#         valor = int(input("Digite o numero da sua escolha: "))
+    try:
+        valor = int(input("Digite o numero da sua escolha: "))
         
-#         if valor == 1:
-#             print("\n-> Voce escolheu: cafe puro")
-#         elif valor == 2:
-#             print("\n-> Voce escolheu: cafe com leite")
-#         elif valor == 3:
-#             print("\n-> Voce escolheu: chocolate")
-#         elif valor == 4:
-#             print("\n-> Voce escolheu: cha")
-#         elif valor == 5:
-#             print("\n-> Voce escolheu: suco")
-#         else:
-#             print("\n-> Opcao invalida")
-#     except ValueError:
-#         print("\n-> Erro: Por favor, digite apenas numeros inteiros.")
+        if valor == 1:
+            print("\n-> Voce escolheu: cafe puro")
+        elif valor == 2:
+            print("\n-> Voce escolheu: cafe com leite")
+        elif valor == 3:
+            print("\n-> Voce escolheu: chocolate")
+        elif valor == 4:
+            print("\n-> Voce escolheu: cha")
+        elif valor == 5:
+            print("\n-> Voce escolheu: suco")
+        else:
+            print("\n-> Opcao invalida")
+    except ValueError:
+        print("\n-> Erro: Por favor, digite apenas numeros inteiros.")
 
-# cafeteria()
+cafeteria()
 
